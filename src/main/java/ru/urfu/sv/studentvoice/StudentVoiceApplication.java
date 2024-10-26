3b3d67f9995698702dd8e7275ce95a89e41dd78b
Egor Sazhin <sazhin2003.egor@gmail.com>
2024-10-26 20:44:16 +0500

    master - Initial commit

---
+++ src/main/java/ru/urfu/sv/studentvoice/StudentVoiceApplication.java
@@ -0,0 +1,13 @@
+package ru.urfu.sv.studentvoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentVoiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentVoiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/urfu/sv/studentvoice/StudentVoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.urfu.sv.studentvoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentVoiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

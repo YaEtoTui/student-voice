011ed53517dd61937dc7729e35ba03c632b18a9a
Egor Sazhin <sazhin2003.egor@gmail.com>
2024-10-28 21:20:54 +0500

    STUD-TEST - Написал тесты на контроллер JwtAuthenticationController

--- src/main/java/ru/urfu/sv/studentvoice/controllers/jwt/JwtAuthenticationController.java
+++ src/main/java/ru/urfu/sv/studentvoice/controllers/jwt/JwtAuthenticationController.java
@@ -1,17 +1,13 @@
 package ru.urfu.sv.studentvoice.controllers.jwt;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
--- src/test/java/ru/urfu/sv/studentvoice/StudentVoiceApplicationTests.java
+++ src/test/java/ru/urfu/sv/studentvoice/StudentVoiceApplicationTests.java
@@ -1,4 +1,4 @@
-package ru.urfu.sv.studentvoice.studentvoice;
+package ru.urfu.sv.studentvoice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,5 +9,4 @@ class StudentVoiceApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }

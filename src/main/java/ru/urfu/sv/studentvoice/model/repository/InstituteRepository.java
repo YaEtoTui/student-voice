ccf93a213dee5382a484f32b845a227efdc0dea6
Egor Sazhin <sazhin2003.egor@gmail.com>
2024-11-30 17:19:12 +0500

    STUD-30 - Рефакторинг repository

--- src/main/java/ru/urfu/sv/studentvoice/model/repository/InstituteRepository.java
+++ src/main/java/ru/urfu/sv/studentvoice/model/repository/InstituteRepository.java
@@ -4,16 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.urfu.sv.studentvoice.model.domain.entity.Institute;
 
-import java.util.Optional;
-
 @Repository
 public interface InstituteRepository extends JpaRepository<Institute, Long> {
-
-    Optional<Institute> findByFullNameIgnoreCase(String fullName);
-
-    Optional<Institute> findByShortNameIgnoreCase(String shortName);
-
-    boolean existsByAddress(String address);
-
-    Optional<Institute> findByAddressIgnoreCase(String address);
 }
--- src/main/java/ru/urfu/sv/studentvoice/model/repository/ReviewRepository.java
+++ src/main/java/ru/urfu/sv/studentvoice/model/repository/ReviewRepository.java
@@ -1,19 +1,9 @@
 package ru.urfu.sv.studentvoice.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.urfu.sv.studentvoice.model.domain.entity.Review;
 
-import java.util.List;
-import java.util.UUID;
-
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-
-//    List<Review> findAllBySessionId(UUID sessionId);
-//
-//    @Query(nativeQuery = true, value = "select * from reviews where session_id in (:ids)")
-//    List<Review> findAllBySessionsIds(@Param("ids") List<UUID> sessionsIds);
 }
